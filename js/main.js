a0bc6f7d6c014e44c2ac27665c022af8e8737f58
Daniel Canales <sergiodanielcanales123@gmail.com>
2022-06-01 18:04:21 -0300

    Todo listo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let nombre = prompt('Hola ¿Cuál es tu nombre?')
+console.log(nombre)
+let edad
+edad = prompt('Cuántos años tenes?')
+console.log(edad)
+
+if(edad) {
+    console.log('Bienvenido')
+}
